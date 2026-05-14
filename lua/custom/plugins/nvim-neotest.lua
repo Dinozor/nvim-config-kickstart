0cd90df93f3107004ae221863c1d188ec517bafe
Dino <dinocrys@gmail.com>
2026-05-14 15:18:43 +0300

    fix: correct neotest-phpunit setup, route phpunit through docker compose exec

--- lua/custom/plugins/nvim-neotest.lua
+++ lua/custom/plugins/nvim-neotest.lua
@@ -1,22 +1,22 @@
 return {
-    "nvim-neotest/neotest",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "antoinemadec/FixCursorHold.nvim",
-        "nvim-treesitter/nvim-treesitter",
-        "olimorris/neotest-phpunit",
-        "nvim-neotest/nvim-nio",
-    },
-    config = function()
-        require("neotest").setup({
-            adapters = {
-                require("neotest-phpunit")
-            },
-        })
-        require("neotest-phpunit")({
-            phpunit_cmd = function()
-                return "vendor/bin/phpunit"
-            end
-        })
-    end
+  'nvim-neotest/neotest',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'antoinemadec/FixCursorHold.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    'olimorris/neotest-phpunit',
+    'nvim-neotest/nvim-nio',
+  },
+  config = function()
+    require('neotest').setup {
+      adapters = {
+        require('neotest-phpunit') {
+          phpunit_cmd = function()
+            local service = vim.g.php_service or 'app'
+            return { 'docker', 'compose', 'exec', '-T', service, 'vendor/bin/phpunit' }
+          end,
+        },
+      },
+    }
+  end,
 }
